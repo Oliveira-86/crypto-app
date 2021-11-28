28418638bb988767b3cc31371ce56dd28ac98efe
oliveira-86 <egberto.duarte.14@gmail.com>
2021-11-28 14:56:52 -0300

    Project create with sidebar menu and navbar components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+  Exchanges,
+} from "./components";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact element={<Homepage />} path="/" />
+              <Route exact element={<Exchanges />} path="exchanges" />
+              <Route
+                exact
+                element={<Cryptocurrencies />}
+                path="cryptocurrencies"
+              />
+              <Route exact element={<CryptoDetails />} path="crypto/:coinId" />
+              <Route exact element={<News />} path="news" />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Copyright © 2021
+            <Link to="/">Cryptoverse Inc.</Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
