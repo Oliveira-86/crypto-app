0bbf0d4b8e1c1a5331d8fa49a1c339769aa0577a
oliveira-86 <egberto.duarte.14@gmail.com>
2021-11-28 21:54:24 -0300

    Create services folder with @reduxjs/toolkit, Cryptocurrencies and News components

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import 'antd/dist/antd.css'
+
+import store from './app/store';
+import { Provider } from 'react-redux'
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <Provider store={store}>
       <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
